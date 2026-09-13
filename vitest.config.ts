a7378261e9714fcf6f5cb29cc57a4d233911d58a
Gabriel Rufino <contato@gabrielrufino.com>
2026-09-12 22:54:18 -0300

    test: exclude stryker temporary directory from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  test: {
+    exclude: [...configDefaults.exclude, '.stryker-tmp/**'],
+  },
   plugins: [
     {
       name: 'vite-plugin-hbs',
